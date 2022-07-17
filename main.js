30c9120ec5c5f60a79fb6905679ab80077626e30
juancotera <juancoteracastro@gmail.com>
2022-07-17 17:34:37 -0500

    proyecto terminado

--- main.js
+++ main.js
@@ -41,27 +41,3 @@ async function challenge() {
 }
 
 challenge()
-
-
-// const render = async () => {
-//   const data = await getResources();
-
-//   data.forEach((item) => {
-//     div.innerHTML += `
-//     <div class="card">
-//       <div class="card-image">
-//         <img class="card-image--background" src="${item.background || 'https://storage.googleapis.com/bucket-larnu/media/business/153/images/BO64E73I.png'}" alt="${item.name}">
-//         <img class="card-image--icon"  src="${item.icon}" alt="${item.name}">
-//       </div>
-//       <div class="card-content">
-//         <h3 class="card-title">${item.name}</h3>
-//         <p class="card-text">Total Quizzes: ${item.totalQuizzes}</p>
-//         <p class="card-text">User: ${item.users}</p>
-//         <a href="https://larnu.app/" target="_blank"  class="btn btn-primary">Go to Larnu</a>
-//       </div>
-//   </div>
-//   `;
-//   });
-// };
-
-// render();
